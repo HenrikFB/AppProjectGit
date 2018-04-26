d16c52d232759491bb10b6532d2992493e9e6f8e
Henrik Fog Bunzel <henrik_1993@hotmail.com>
2018-04-26 10:22:54 +0200

    Before i change profilActivity to AppCompatActivity

--- app/src/main/java/com/example/henrikfogbunzel/appproject/ProfileActivity.java
+++ app/src/main/java/com/example/henrikfogbunzel/appproject/ProfileActivity.java
@@ -1,15 +1,22 @@
 package com.example.henrikfogbunzel.appproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends FragmentActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
@@ -18,10 +25,41 @@ public class ProfileActivity extends FragmentActivity implements BottomNavigatio
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        ((AppCompatActivity)FragmentActivity()).setSupportActionBar(toolbar);
+        //((AppCompatActivity)getApplicationContext()).setSupportActionBar(toolbar);
+
+
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
         loadFragment(new CameraFragment());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuLogout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+
+        return true;
     }
 
     private boolean loadFragment(Fragment fragment){
